0347544f1e39fecb9a0ae325d6cfe40fc5a8fa20
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2026-07-18 10:59:47 +0800

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import './style.css';
+  import './app.css';
+  import logo from './assets/images/logo-universal.png';
+  import { Greet } from '../wailsjs/go/main/App';
+
+  let name = '';
+  let result = 'Please enter your name below 👇';
+
+  function greet() {
+    if (!name) return;
+    Greet(name)
+      .then((res) => { result = res; })
+      .catch((err) => console.error(err));
+  }
+</script>
+
+<img id="logo" class="logo" src={logo} alt="logo" />
+<div class="result" id="result">{result}</div>
+<div class="input-box" id="input">
+  <input class="input" id="name" type="text" autocomplete="off" bind:value={name} />
+  <button class="btn" onclick={greet}>Greet</button>
+</div>
